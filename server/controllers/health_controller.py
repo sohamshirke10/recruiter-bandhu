f68b5ff4c08ba5e3354c3d2af5589c45fa6d49a8
sohamshirke10 <sohamshirke100304@gmail.com>
2025-06-15 00:03:28 +0530

    adding env and venv to gotignore

--- server/controllers/health_controller.py
+++ server/controllers/health_controller.py
@@ -1,10 +1,11 @@
 from flask import jsonify
 from services.health_service import HealthService
 
+
 class HealthController:
     def __init__(self):
         self.health_service = HealthService()
 
     def check_health(self):
         health_status = self.health_service.check_health()
-        return jsonify(health_status), 200 
+        return jsonify(health_status), 200
--- server/routes/health_routes.py
+++ server/routes/health_routes.py
@@ -1,9 +1,10 @@
 from flask import Blueprint
 from controllers.health_controller import HealthController
 
-health_bp = Blueprint('health', __name__)
+health_bp = Blueprint("health", __name__)
 health_controller = HealthController()
 
-@health_bp.route('/health', methods=['GET'])
+
+@health_bp.route("/health", methods=["GET"])
 def health_check():
-    return health_controller.check_health() 
+    return health_controller.check_health()
